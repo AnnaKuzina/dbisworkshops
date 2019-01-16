571350f8aff712ef26dd9ea7f18a926a9c1ffee8
Anna <koshkacd@gmail.com>
2019-01-16 14:12:49 +0200

    Update main.py

--- km-51/Kuzina_Anna/workshop3/source/main.py
+++ km-51/Kuzina_Anna/workshop3/source/main.py
@@ -2,7 +2,29 @@
 Тут написати умову до завдання
 """
 
+from flask import Flask, render_template, request, make_response, session, redirect, url_for, flash
 
 
-if __name__ == "__main__":
-    #TODO
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template("index1.html")
+
+
+
+@app.route('/api', methods=['POST'])
+def apiPost():
+    if request.form["action"] == "sum":
+        a = request.form["A"]
+        b = request.form["B"]
+        response = make_response(redirect(url_for('index')))
+        response.set_cookie("result", str(int(a) + int(b)))
+        return response
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
